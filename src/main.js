2242bc14f5cee98e6026e3a0b44b7d879ac5d55d
Generalr3d <tdrdingren@gmail.com>
2026-01-21 20:45:32 +0800

    deploy yaml file change

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 import Game from './Game.js'
 
+// Dynamically load Ammo.js
+const script = document.createElement('script');
+script.src = import.meta.env.BASE_URL + 'js/ammo.wasm.js';
+document.body.appendChild(script);
+
 window.game = new Game(document.querySelector('canvas.webgl'))
